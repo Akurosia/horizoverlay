d8f9d2c23a10eb7b62d4b3169d56e3dfbece705a
root <root@akurosia.de>
2021-12-05 12:15:29 +0000

    merged all original prs into my fork

--- src/Config.js
+++ src/Config.js
@@ -259,6 +259,7 @@ class ConfigRaw extends Component {
                   onChange={this.handleConfig}
                 />
               </div>
+            </div>
           </fieldset>
           <fieldset className="fieldsZoom">
             <legend>
