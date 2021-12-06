fbf5fc5c8e30152c5960bb765786d58364080c29
Akurosia <kamothegamer@web.de>
2021-12-06 11:58:48 +0100

    Updates

---
+++ precache-manifest.ac3d678cd79fadaa99ac570c3f3609e8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30ea3accf6e6cc17b11aae0931cb6acb",
+    "url": "/horizoverlay/index.html"
+  },
+  {
+    "revision": "9d0e699341faf58ed90c",
+    "url": "/horizoverlay/static/css/main.d6bf0480.chunk.css"
+  },
+  {
+    "revision": "f089bdea352376d34ee4",
+    "url": "/horizoverlay/static/js/2.50281b13.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/horizoverlay/static/js/2.50281b13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d0e699341faf58ed90c",
+    "url": "/horizoverlay/static/js/main.89a29aed.chunk.js"
+  },
+  {
+    "revision": "30f65308d7bdcba22167",
+    "url": "/horizoverlay/static/js/runtime-main.9a241b58.js"
+  },
+  {
+    "revision": "e1e188201661cad8e440f57279b85a23",
+    "url": "/horizoverlay/static/media/mog.e1e18820.png"
+  },
+  {
+    "revision": "fda2c25cdb67dbb063acb7e576ecd60a",
+    "url": "/horizoverlay/static/media/rpr.fda2c25c.png"
+  },
+  {
+    "revision": "750ecccac0954992127557220e8a3bfd",
+    "url": "/horizoverlay/static/media/sge.750eccca.png"
+  }
+]);
